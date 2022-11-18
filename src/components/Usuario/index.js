ec200c954a4bceb28385b76ba233057f2fc29d27
gsnorberto <sn_gabriel@outlook.com>
2022-11-18 08:22:47 -0300

    Versão permite trocar o nome de usuário e a foto do perfil

--- src/components/Usuario/index.js
+++ src/components/Usuario/index.js
@@ -1,12 +1,42 @@
+import { useState } from "react";
+
 export const Usuario = () => {
+    const [userName, setUserName] = useState('Fiona');
+    const [userImage, setUserImage] = useState('./media/imgs/perfil.png');
+    let newUserName;
+    let newUserImage;
+
+    function enterName(e) {
+        e.preventDefault();
+
+        let aux = prompt("Qual seu nome?");
+        if (aux) {
+            newUserName = aux;
+            setUserName(newUserName);
+        }
+    }
+
+    function enterImage(e) {
+        e.preventDefault();
+
+        let aux = prompt("Insira a URL de uma imagem");
+        if (aux) {
+            newUserImage = aux;
+            setUserImage(newUserImage);
+        }
+    }
+
     return (
         <div className="owner-profile">
-            <a href=""><img src="./media/imgs/perfil.png" alt="" /></a>
+            <a href=""><img onClick={enterImage} src={userImage} alt="" /></a>
 
             <div className="desc-profile">
                 <a href="">
                     <span>Shrek</span>
-                    Fiona
+                    <div>
+                        {userName}
+                        <ion-icon onClick={enterName} name="pencil-outline"></ion-icon>
+                    </div>
                 </a>
             </div>
         </div>
